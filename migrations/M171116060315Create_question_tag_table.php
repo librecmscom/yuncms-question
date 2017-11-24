386ab82f469f620163af2099406f16aae9fbc821
Peter <xutongle@gmail.com>
2017-11-24 17:41:47 +0800

    update

--- migrations/M171116060315Create_question_tag_table.php
+++ migrations/M171116060315Create_question_tag_table.php
@@ -12,7 +12,7 @@ class M171116060315Create_question_tag_table extends Migration
         $tableOptions = null;
         if ($this->db->driverName === 'mysql') {
             // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
-            $tableOptions = 'CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE=InnoDB';
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
         }
         $this->createTable('{{%question_tag}}', [
             'question_id' => $this->integer()->unsigned()->notNull()->comment('Question ID'),
